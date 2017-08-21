13bfc133a6a116bd6d8b3a80e9a7cc8b870fac6e
Juha Erkkilä <je@turnipsi.no-ip.org>
2017-08-21 21:58:28 +0300

    Change license from public domain to standard OpenBSD license. Obviously the previous versions are still in public domain... The intent is the same, but I suppose this wording is less ambiguous is most jurisdictions.

--- Praati/View/L10N.pm
+++ Praati/View/L10N.pm
@@ -1,7 +1,19 @@
 # -*- mode: perl; coding: iso-8859-1; -*-
 # $Id: L10N.pm,v 1.12 2014/06/21 08:08:12 je Exp $
 #
-# The author disclaims copyright to this source code.
+# Copyright (c) 2014, 2017 Juha Erkkilä <je@turnipsi.no-ip.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 use strict;
 use warnings FATAL => qw(all);
